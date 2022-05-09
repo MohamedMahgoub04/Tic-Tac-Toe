a7b061140c5cc9ea89767f5cc9c92092ef017640
Mohamed <M.Mahgoub04@gmail.com>
2022-05-10 01:01:31 +0400

    Find winner (row and column)
    Add restart button

--- script.js
+++ script.js
@@ -14,22 +14,12 @@ const gameBoard = (() => {
 
 const displayController = (() => {
 
- // const turn = () => {
- //  let lastPlayed = Player2
- //  const switchTurn = () => {
- //   if (lastPlayed == Player2){
- //    lastPlayed = Player1
- //   } else{
- //    lastPlayed = Player2
- //   }
- //  }
-
- //  return { lastPlayed, switchTurn }
- // }
-
  var lastPlayed = Player2
+ var gameOn = true
 
  const display = () => {
+  
+
   for (row of gameBoard.array){
    let rowIndex = gameBoard.array.indexOf(row)
    let i = 0
@@ -40,35 +30,114 @@ const displayController = (() => {
    const div = document.createElement('div')
    const h1 = document.querySelector('h1')
    div.className = 'box'
+   div.setAttribute('id', `${rowIndex}-${boxIndex}`)
    div.innerHTML = `<p class='mark'>${box}</p>`
 
+   
    div.addEventListener('click', () => {
-    // if(box == ' '){
+    if(gameOn){
      if (displayController.lastPlayed === Player2){
-      // alert(box)
+      
+      console.log(`${rowIndex}, ${boxIndex}`)
+      div.className = 'box played'
      h1.innerHTML = "Player O's turn"
      grid.innerHTML = ''
      gameBoard.array[rowIndex][boxIndex] = Player1.mark
      displayController.display()
      displayController.lastPlayed = Player1
+     isGameOver(Player1)
+     
     } else {
+     console.log(`${rowIndex}, ${boxIndex}`)
      h1.innerHTML = "Player X's turn"
      grid.innerHTML = ''
      gameBoard.array[rowIndex][boxIndex] = Player2.mark
      displayController.display()
      displayController.lastPlayed = Player2
+     div.className = 'box played'
+     isGameOver(Player2)
     }
-    // }
+    // } else {
+    // //  console.log(`${rowIndex}, ${boxIndex}`)
+    //  alert()
+    }
    })
 
    grid.append(div)
   }
  }
+  return {gameOn}
  }
 
+ const isGameOver = (player) => {
+ 
+  for (let j = 0; j <= 2; j++){
+   // Checks for rows and columns
+   let rowWin = 0
+   let colwin = 0
+   
+
+   for (let i = 0; i <= 2; i++){
+    if (gameBoard.array[j][i] == player.mark){
+     rowWin++
+    }
+   }
+   for (let i = 0; i <= 2; i++){
+    if (gameBoard.array[i][j] == player.mark){
+     colwin++
+    }
+   }
+   
+
+   // Game Over
+   if (rowWin == 3 || colwin == 3){
+    gameOn = false
+    const h4 = document.createElement('h4')
+    const result = document.createElement('div')
+    result.className = 'result'
+    h4.innerHTML = `Player ${player.mark} has won!`
+    result.append(h4)
+    document.querySelector('#result-div').append(result)
+   }  
+  }
+
+  // let diawin = 0
+  // for (let i = 0, k = 0; i <= 2, k <= 2; i++, k++){
+  //   if (k == i){
+  //    if (gameBoard.array[i][k] == player.mark){
+  //     diawin++
+  //    }
+  //   }
+  //  }
+  //  if (diawin == 3){
+  //   gameOn = false
+  //   const h4 = document.createElement('h4')
+  //   const result = document.createElement('div')
+  //   result.className = 'result'
+  //   h4.innerHTML = `Player ${player.mark} has won!`
+  //   result.append(h4)
+  //   document.querySelector('#result-div').append(result)
+  //  }  
+
+ }
+
+ const restart = () => {
+  const btn = document.querySelector('#restart')
+  const grid = document.querySelector('#grid')
+  
+  btn.onclick = () => {
+   let result = document.querySelector('.result')
+   result.remove()
+   lastPlayed = Player2
+   gameOn = true
+   gameBoard.array = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']]
+   grid.innerHTML = ''
+   display()
+  }
+ }
  
 
- return { display, lastPlayed }
+ return { display, lastPlayed, restart }
 })()
 
 
@@ -76,6 +145,7 @@ const displayController = (() => {
 document.addEventListener('DOMContentLoaded', () => {
 
  displayController.display()
+ displayController.restart()
  
 
 })
