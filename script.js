b4aedbd7f9e577450ba7bfff8ac185fe5350fb8c
Mohamed <M.Mahgoub04@gmail.com>
2022-05-10 23:22:01 +0400

    Find winner-mAdd favicon

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const Player1 = Player('X')
 const Player2 = Player('O')
 
 const gameBoard = (() => {
- let array = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']]
+ let array = [['','',''],['','',''],['','','']]
 
  return { array }
 })()
@@ -18,7 +18,6 @@ const displayController = (() => {
  var gameOn = true
 
  const display = () => {
-  
 
   for (row of gameBoard.array){
    let rowIndex = gameBoard.array.indexOf(row)
@@ -32,34 +31,26 @@ const displayController = (() => {
    div.className = 'box'
    div.setAttribute('id', `${rowIndex}-${boxIndex}`)
    div.innerHTML = `<p class='mark'>${box}</p>`
-
    
    div.addEventListener('click', () => {
-    if(gameOn){
+    if (gameOn && div.innerHTML === '<p class="mark"></p>'){
      if (displayController.lastPlayed === Player2){
-      
-      console.log(`${rowIndex}, ${boxIndex}`)
       div.className = 'box played'
-     h1.innerHTML = "Player O's turn"
-     grid.innerHTML = ''
-     gameBoard.array[rowIndex][boxIndex] = Player1.mark
-     displayController.display()
-     displayController.lastPlayed = Player1
-     isGameOver(Player1)
-     
-    } else {
-     console.log(`${rowIndex}, ${boxIndex}`)
-     h1.innerHTML = "Player X's turn"
-     grid.innerHTML = ''
-     gameBoard.array[rowIndex][boxIndex] = Player2.mark
-     displayController.display()
-     displayController.lastPlayed = Player2
-     div.className = 'box played'
-     isGameOver(Player2)
+      h1.innerHTML = "Player O's turn"
+      grid.innerHTML = ''
+      gameBoard.array[rowIndex][boxIndex] = Player1.mark
+      displayController.display()
+      displayController.lastPlayed = Player1
+      isGameOver(Player1)
+     } else {
+      h1.innerHTML = "Player X's turn"
+      grid.innerHTML = ''
+      gameBoard.array[rowIndex][boxIndex] = Player2.mark
+      displayController.display()
+      displayController.lastPlayed = Player2
+      div.className = 'box played'
+      isGameOver(Player2)
     }
-    // } else {
-    // //  console.log(`${rowIndex}, ${boxIndex}`)
-    //  alert()
     }
    })
 
@@ -72,10 +63,12 @@ const displayController = (() => {
  const isGameOver = (player) => {
  
   for (let j = 0; j <= 2; j++){
-   // Checks for rows and columns
+   // Checks for rows, columns and diagonals
    let rowWin = 0
    let colwin = 0
-   
+   let dia1win = 0
+   let dia2win = 0
+   let emptyCount = 0
 
    for (let i = 0; i <= 2; i++){
     if (gameBoard.array[j][i] == player.mark){
@@ -87,10 +80,28 @@ const displayController = (() => {
      colwin++
     }
    }
-   
+   for (let i = 0, k = 0; i <= 2, k <= 2; i++, k++){
+     if (k == i){
+      if (gameBoard.array[i][k] == player.mark){
+       dia1win++
+      }
+     }
+   } 
+   for (let i = 0, k = 2; i <= 2, k >= 0; i++, k--){
+      if (gameBoard.array[i][k] == player.mark){
+       dia2win++
+      }
+   } 
+   for (row of gameBoard.array){
+    for (box of row){
+     if (box == ""){
+      emptyCount++
+     }
+    }
+   }
 
    // Game Over
-   if (rowWin == 3 || colwin == 3){
+   if (rowWin == 3 || colwin == 3 || dia1win == 3 || dia2win == 3){
     gameOn = false
     const h4 = document.createElement('h4')
     const result = document.createElement('div')
@@ -98,27 +109,18 @@ const displayController = (() => {
     h4.innerHTML = `Player ${player.mark} has won!`
     result.append(h4)
     document.querySelector('#result-div').append(result)
-   }  
+    break
+   } else if(emptyCount == 0){
+    gameOn = false
+    const h4 = document.createElement('h4')
+    const result = document.createElement('div')
+    result.className = 'result'
+    h4.innerHTML = `It's a tie!`
+    result.append(h4)
+    document.querySelector('#result-div').append(result)
+    break
+   }
   }
-
-  // let diawin = 0
-  // for (let i = 0, k = 0; i <= 2, k <= 2; i++, k++){
-  //   if (k == i){
-  //    if (gameBoard.array[i][k] == player.mark){
-  //     diawin++
-  //    }
-  //   }
-  //  }
-  //  if (diawin == 3){
-  //   gameOn = false
-  //   const h4 = document.createElement('h4')
-  //   const result = document.createElement('div')
-  //   result.className = 'result'
-  //   h4.innerHTML = `Player ${player.mark} has won!`
-  //   result.append(h4)
-  //   document.querySelector('#result-div').append(result)
-  //  }  
-
  }
 
  const restart = () => {
@@ -127,15 +129,17 @@ const displayController = (() => {
   
   btn.onclick = () => {
    let result = document.querySelector('.result')
-   result.remove()
+   let resultDiv = document.querySelector('#result-div')
+   if (resultDiv.innerHTML != ''){
+    result.remove()
+   }
    lastPlayed = Player2
    gameOn = true
-   gameBoard.array = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']]
+   gameBoard.array = [['','',''],['','',''],['','','']]
    grid.innerHTML = ''
    display()
   }
  }
- 
 
  return { display, lastPlayed, restart }
 })()
@@ -146,6 +150,5 @@ document.addEventListener('DOMContentLoaded', () => {
 
  displayController.display()
  displayController.restart()
- 
 
 })
